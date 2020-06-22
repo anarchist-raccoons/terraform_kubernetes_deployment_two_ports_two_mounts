3b682910fabe50ccd7db12f45ff7e92fafb89902
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2020-06-22 10:21:19 +0000

    config for two ports and two mount points to a single volume

---
+++ main.tf
@@ -0,0 +1,106 @@
+provider "kubernetes" {
+  load_config_file = false
+  host = "${var.host}"
+  username = "${var.username}"
+  password = "${var.password}"
+  client_certificate = "${var.client_certificate}"
+  client_key = "${var.client_key}"
+  cluster_ca_certificate = "${var.cluster_ca_certificate}"
+}
+
+resource "kubernetes_deployment" "default" {
+  
+  metadata {
+    name = "${var.app_name}"
+    labels = {
+      app = "${var.app_name}"
+    }
+  }
+
+  spec {
+    replicas = "${var.replicas}"
+  
+    selector {
+      match_labels = {
+        app = "${var.app_name}"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "${var.app_name}"
+        }
+      }
+
+      spec {
+        image_pull_secrets { 
+          name = "${var.image_pull_secrets}"
+        }
+
+        container {
+          image = "${var.docker_image}"
+          name = "${var.app_name}"
+
+          env_from {
+            secret_ref {
+              name = "${var.env_from}"
+            }
+          }
+          
+          volume_mount {
+            name = "${var.app_name}"
+            mount_path = "${var.primary_mount_path}"
+          }
+          volume_mount {
+            name = "${var.sapp_name}"
+            mount_path = "${var.secondary_mount_path}"
+            sub_path = "${var.secondary_sub_path}"
+          }
+
+          command = "${var.command}"
+        }
+
+        volume {
+          name = "${var.app_name}"
+          persistent_volume_claim {
+          claim_name = "${var.pvc_claim_name}"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "default" {
+  metadata {
+    name = "${var.app_name}"
+    labels = {
+      app = "${var.app_name}"
+    }
+  }
+  spec {
+    selector = {
+      app = "${kubernetes_deployment.default.metadata.0.labels.app}"
+    }
+    session_affinity = "ClientIP"
+    
+    port {
+      name        = "primary-port"
+      port        = "${var.primary_port}"
+      target_port = "${var.primary_port}"
+    }
+
+    port {
+      name        = "secondary-port"
+      port        = "${var.secondary_port}"
+      target_port = "${var.secondary_port}"
+    }
+
+    
+    load_balancer_ip = "${var.load_balancer_ip}"
+    load_balancer_source_ranges = "${var.load_balancer_source_ranges}"
+
+    type = "${var.service_type}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "host" { }
+variable "username" { }
+variable "password" { }
+variable "client_certificate" { }
+variable "client_key" { }
+variable "cluster_ca_certificate" { }
+variable "docker_image" { }
+variable "app_name" { }
+variable "primary_port" { }
+variable "secondary_port" { }
+
+variable "env_from" { }
+
+variable "command" {
+  default = []
+}
+
+variable "primary_mount_path" {
+}
+variable "secondary_mount_path" {
+}
+
+variable "secondary_sub_path" {
+  default = ""
+}
+
+variable "image_pull_secrets" {
+}
+
+variable "resource_version" {
+  description = "Unused variable, used to create a dependency sequence."
+  type = "list"
+  default = []
+}
+
+variable "pvc_claim_name" {}
+variable "secondary_pvc_claim_name" {
+  default = ""
+  }
+
+variable "replicas" {
+  default = 1
+}
+
+variable "load_balancer_source_ranges" {
+  type = "list"
+  default = []
+}
+
+variable "service_type" {
+  default = "LoadBalancer"
+}
+
+variable "load_balancer_ip" {
+  default = ""
+}
